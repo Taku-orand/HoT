61249ff2857a39f9f47c68caa63a219cdc9ea352
s1270182 <uoastudent182@gmail.com>
2020-08-06 20:34:43 +0900

    HoT: Initial revision

---
+++ hot.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#define ROUND 3
+
+int main(){
+    int i=0,head=0,tail=0;
+    srand((unsigned int)time(NULL));
+
+    printf("Tossing a coin...\n");
+
+    for(i=1;i<=ROUND;i++){
+        if(rand()%2 == 0){
+            printf("Round %d: Heads\n",i);
+            head++;
+        }else{
+            printf("Round %d: Tails\n",i);
+            tail++;
+        }
+    }
+    printf("Heads: %d, Tails: %d\n",head,tail);
+
+    return 0;
+}
